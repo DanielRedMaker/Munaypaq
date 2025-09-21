1848a909d542b34c24fa77e80e6705f96a017b63
DanielRedMaker <danielred20a107@gmail.com>
2025-09-20 21:00:32 -0500

    Se organizo mejor
    Mejor organizado

--- Munaypaq/Assets/Tiles/BadNPC.cs
+++
@@ -1,8 +0,0 @@
-public class BadNPC : NPCBase
-{
-    protected override void PerformAction()
-    {
-        // Ensuciar tile actual
-        GridManager.Instance.SetTileState(currentGridPosition, TileState.Dirty);
-    }
-}
--- Munaypaq/Assets/Tiles/GoodNPC.cs
+++
@@ -1,8 +0,0 @@
-public class GoodNPC : NPCBase
-{
-    protected override void PerformAction()
-    {
-        // Limpiar tile actual
-        GridManager.Instance.SetTileState(currentGridPosition, TileState.Clean);
-    }
-}
